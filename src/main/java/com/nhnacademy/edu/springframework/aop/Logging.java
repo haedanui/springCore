cad6de56e001d2f5605962cb33efdf89ba2ca09c
haedanui <rhs9705kr@naver.com>
2024-02-01 15:47:39 +0900

    AnnotationConfigApplicationContext + JavaConfig + @Bean 사용

--- src/main/java/com/nhnacademy/edu/springframework/aop/Logging.java
+++ src/main/java/com/nhnacademy/edu/springframework/aop/Logging.java
@@ -14,7 +14,7 @@ public class Logging {
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
 
-        Object target = null;
+        Object target;
         try{
             target=pjp.proceed();
         }catch (Exception e){
