936140102ffe6aed3dc8c8ed008b50b529146961
haedanui <rhs9705kr@naver.com>
2024-02-02 15:53:32 +0900

    커스텀 어노테이션 추가

--- src/main/java/com/nhnacademy/edu/springframework/config/SenderConfig.java
+++ src/main/java/com/nhnacademy/edu/springframework/config/SenderConfig.java
@@ -1,10 +1,6 @@
 package com.nhnacademy.edu.springframework.config;
 
 import com.nhn.dooray.client.DoorayHookSender;
-import com.nhnacademy.edu.springframework.aop.Logging;
-import com.nhnacademy.edu.springframework.sender.MessageSender;
-import com.nhnacademy.edu.springframework.sender.impl.DoorayMessageSender;
-import com.nhnacademy.edu.springframework.service.MessageSendService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import org.springframework.web.client.RestTemplate;
